5d33bacd28d95187e3a174b188c5e633d42c01ee
Dorian Lenzner <iwantmyfingerback@gmail.com>
2016-07-12 15:12:53 +0200

    ready to move that to main script

--- argparser/argparser_gist.py
+++ argparser/argparser_gist.py
@@ -7,7 +7,7 @@ __author__ = 'Dorian Lenzner'
 def get_args():
     parser = argparse.ArgumentParser(description='Retrieves arguments')
 
-    parser.add_argument('-a', '--adress', type=str, help='Ip Adresses seperated by space', required=False, nargs='*')
+    parser.add_argument('-a', '--adress', type=str, help='Ip Adresses seperated by ,', required=False, nargs='+')
     parser.add_argument('-f', '--file', type=str, help='File with IPs, line by line', required=False)
 
     args = parser.parse_args()
