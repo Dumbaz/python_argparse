c32afb62d5bb51b24a12d4cf7109d1bf6e9e2281
Dorian Lenzner <iwantmyfingerback@gmail.com>
2016-07-12 11:00:42 +0200

    Added two arguments and defaulting to print_help()

--- argparser/argparser.py
+++ argparser/argparser.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
 from argparse import ArgumentParser
+import sys
 
 parser = ArgumentParser()
 
-parser.add_argument("echo", help="echo the string")
+parser.add_argument("-a" , help="directly input IPs seperated by spaces")
+parser.add_argument("-f", help="input a file with IPs, one IP per line")
 
+if len(sys.argv)==1:
+    parser.print_help()
+    sys.exit(1)
 
 args = parser.parse_args()
 print(args.echo)
